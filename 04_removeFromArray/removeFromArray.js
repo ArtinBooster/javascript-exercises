1bf7e0af53a7ad8e5edf511e4a249d68692c5995
artinBooster <125482486+ArtinBooster@users.noreply.github.com>
2025-04-20 16:47:35 +0330

    Do "removeFromArray" exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(customArray, ...args) {
+    const newArray = [];
+    for (item of customArray) {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
