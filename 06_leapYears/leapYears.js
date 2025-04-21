59f929541c3534a8c70f57b3b2730f36b3d09761
artinBooster <125482486+ArtinBooster@users.noreply.github.com>
2025-04-21 19:21:40 +0330

    Do "leapYears" exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(givenYear) {
+    const isYearDivBy4 = givenYear % 4 == 0;
+    const isYearDivBy400 = givenYear % 400 == 0;
+    const isYearCentury = givenYear % 100 == 0;
+    if (
+        isYearDivBy4 &&
+        (!isYearCentury || isYearDivBy400)
+    ) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
