4397efcf9115d01db033946ba4318c12eebf9d05
artinBooster <125482486+ArtinBooster@users.noreply.github.com>
2025-04-23 21:25:27 +0330

    Do "palindromes" exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let originalStr = [];
+    let reversedStr = [];
 
+    str.toLowerCase().split("").forEach(
+        (letter) => {
+            if (
+                letter != " " &&
+                letter != "!" &&
+                letter != "," &&
+                letter != "."
+            ) {
+                originalStr.push(letter);
+                reversedStr.push(letter);
+            }
+        }
+    )
+
+    originalStr = originalStr.join("");
+    reversedStr = reversedStr.reverse().join("");
+
+    return (originalStr == reversedStr);
 };
 
 // Do not edit below this line
