13d81a113f422aae71c06e1e04ad42a1cba0f5fd
artinBooster <125482486+ArtinBooster@users.noreply.github.com>
2025-04-23 22:01:10 +0330

    Do "fibonacci" exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (+num < 0) return "OOPS";
+    if (+num == 0) return 0; 
+    
+    let answer = [];
 
+    for (let i = 0; i < +num; i++) {
+        if (i == 0) {
+            answer.push(1);
+            continue;
+        }
+        if (i == 1) {
+            answer.push(1);
+            continue;
+        }
+
+        let nextNumber = answer[i - 2] + answer[i - 1];
+        answer.push(nextNumber);
+    }
+
+    return answer[num - 1];
 };
 
 // Do not edit below this line
