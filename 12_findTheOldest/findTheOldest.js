e399c2bb1e93571aad14d45b3e050b22e86c953c
artinBooster <125482486+ArtinBooster@users.noreply.github.com>
2025-04-24 11:22:55 +0330

    Do "findTheOldest" exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return (death - birth);
+}
 
+const findTheOldest = function(peopleList) {
+    let oldestAge = 0;
+    let oldest;
+
+    for (person of peopleList) {
+        // Find this person's age
+        let personAge = getAge(
+            person.yearOfBirth,
+            person.yearOfDeath
+        );
+
+        if (personAge > oldestAge) {
+            // If this person's age was more than our current highest age it becomes the highest age
+            oldestAge = personAge;
+            oldest = person;
+        }
+    }
+    
+    return oldest;
 };
 
 // Do not edit below this line
